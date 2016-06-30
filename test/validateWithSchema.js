b691c92476942a5cdbb93eea63b98ffc99f33e61
Luc Patiny <luc@patiny.com>
2016-06-30 09:16:27 +0200

    Add test case validating all the examples

---
+++ test/validateWithSchema.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var validate = require('jsonschema').validate;
+
+var fs=require('fs');
+
+
+describe.only('Validate various json chart format', function () {
+    var schemaText=fs.readFileSync('./chart-schema.json', {encoding:'utf8'});
+    
+    var dir=fs.readdirSync('./examples');
+    
+    dir.forEach(function(file) {
+        var jsonChart=JSON.parse(fs.readFileSync('./examples/'+file));
+        it('validating: '+file, function () {
+            var schema=JSON.parse(schemaText);
+            var result=validate(jsonChart, schema);
+            result.errors.length.should.equal(0);
+        });
+    });
+});
